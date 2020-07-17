967768ccacf71ce8e169e3ca7fd7ea3a0290494f
estarodubov <estarodubov@team.amocrm.com>
2020-07-18 02:25:42 +0300

    First Editting

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Info extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Погода</h2>
+                <p>Узнайте погоду в Вашем городе!</p>
+            </div>
+        )
+    }
+}
+
+export default Info;
